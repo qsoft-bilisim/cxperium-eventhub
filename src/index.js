d9667ddf3f6b2168eb762b21487204121fe9554b
Nuri YILMAZ <dengere@gmail.com>
2025-01-27 16:05:44 +0300

    VS Code debuging capabilities

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+});
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
